edd0646ba157ab9d061247fed7b054cfdedfb2d5
me <elmasri.official@gmail.com>
2025-06-21 20:33:47 +0300

    02-09 - Augmented Assignment Operators

---
+++ 02_Primitive_Types/02-09_Augmented_Assignment_Operators.py
@@ -0,0 +1,21 @@
+# Normal Workflow
+#
+# x = 5
+# y = x + 1
+# z = y * 2
+#
+# print(z)
+
+# Shorten
+
+# x = 5
+# x = x + 1
+# x = x * 2
+# print(x)
+
+# Augmented Assignment Operators
+
+x = 5
+x += 1
+x *= 2
+print(x)
