5e3dec2eb5bf37aec9fae49849c96e20fda57dfb
me <elmasri.official@gmail.com>
2025-06-21 20:33:32 +0300

    02-08 - Inputs

---
+++ 02_Primitive_Types/02-08_Inputs.py
@@ -0,0 +1,7 @@
+# Ask user for name and age, then show their age next year
+
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+print(f"Hello {name}, you are {age} years old.")
+print(f"Next year, you will be {age + 1} years old.")
