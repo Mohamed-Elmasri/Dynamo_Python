2fd628b207aa9c5f258619d25926863b9309c456
me <elmasri.official@gmail.com>
2025-07-27 00:07:35 +0300

    03-06

---
+++ 03_Collections/03-06_Tuple.py
@@ -0,0 +1,9 @@
+week_ends = ("Fri", "Sat")
+gender = ("Male", "Female")
+
+week_ends_list = list(week_ends)
+print(week_ends)
+
+week_ends_list.append("Sun")
+week_ends_tuple = tuple(week_ends_list)
+print(week_ends_tuple)
